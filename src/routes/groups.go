ba2004d01503f9ee06ab4cd19403a6b04a70ff6c
Arnav Gupta <dev@championswimmer.in>
2025-05-17 21:03:47 +0100

    fix
    Signed-off-by: Arnav Gupta <dev@championswimmer.in>

--- src/routes/groups.go
+++ src/routes/groups.go
@@ -80,7 +80,7 @@ func createGroup(ctx *fiber.Ctx) error {
 func updateGroup(ctx *fiber.Ctx) error {
 	groupID := ctx.Params("groupIdOrCode")
 
-	group, err := groupsController.GetGroupByIDorCode(groupID, false)
+	group, err := groupsController.GetGroupByIDorCode(groupID)
 	if err != nil {
 		return ctx.Status(err.(*fiber.Error).Code).JSON(dto.CreateErrorResponse(err.(*fiber.Error).Code, err.Error()))
 	}
@@ -120,7 +120,7 @@ func joinGroup(ctx *fiber.Ctx) error {
 	user := ctx.Locals(config.LOCALS_USER).(*models.User)
 	groupIDOrCode := ctx.Params("groupIdOrCode")
 
-	group, err := groupsController.GetGroupByIDorCode(groupIDOrCode, false)
+	group, err := groupsController.GetGroupByIDorCode(groupIDOrCode)
 	if err != nil {
 		return ctx.Status(err.(*fiber.Error).Code).JSON(dto.CreateErrorResponse(err.(*fiber.Error).Code, err.Error()))
 	}
@@ -161,7 +161,7 @@ func leaveGroup(ctx *fiber.Ctx) error {
 	user := ctx.Locals(config.LOCALS_USER).(*models.User)
 	groupIDOrCode := ctx.Params("groupIdOrCode")
 
-	group, err := groupsController.GetGroupByIDorCode(groupIDOrCode, false)
+	group, err := groupsController.GetGroupByIDorCode(groupIDOrCode)
 	if err != nil {
 		return ctx.Status(err.(*fiber.Error).Code).JSON(dto.CreateErrorResponse(err.(*fiber.Error).Code, err.Error()))
 	}
